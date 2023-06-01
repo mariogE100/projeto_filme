d9501f172dacbde41a9007b9821b47c08d8992ea
Mario Henrique <101577344+mariogE100@users.noreply.github.com>
2023-06-01 09:45:07 -0300

    Update footer.php

--- projeto_filme/templates/footer.php
+++ projeto_filme/templates/footer.php
@@ -1,9 +1,9 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" 
-integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous" 
-referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js" 
-integrity="sha512-1/RvZTcCDEUjY/CypiMz+iqqtaoQfAITmNSJY17Myp4Ms5mdxPS5UV7iOfdZoxcGhzFbOm6sntTKJppjvuhg4g==" crossorigin="anonymous" 
-referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+    integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js"
+    integrity="sha512-1/RvZTcCDEUjY/CypiMz+iqqtaoQfAITmNSJY17Myp4Ms5mdxPS5UV7iOfdZoxcGhzFbOm6sntTKJppjvuhg4g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <footer id="footer">
     <div id="social-container">
@@ -19,15 +19,12 @@ referrerpolicy="no-referrer"></script>
             </li>
         </ul>
     </div>
-    <div class="footer-links-container">
+    <div id="footer-links-container">
         <ul>
             <li><a href="#">Adicionar filme</a></li>
-            <li><a href="#">Adicionar criticas</a></li>
-            <li><a href="#">Entrar / Registar</a></li>
+            <li><a href="#">Adicionar crítica</a></li>
+            <li><a href="#">Entrar / Registrar</a></li>
         </ul>
     </div>
-    <p>&copu Senac 2023</p>
+    <p>&copy; Senac 2023</p>
 </footer>
-
-</body>
-</html>
