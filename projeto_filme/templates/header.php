b95f27856df2781b19c29afbd3a0f1d10c9cbb41
Mario Henrique <101577344+mariogE100@users.noreply.github.com>
2023-06-01 09:44:41 -0300

    Update header.php

--- projeto_filme/templates/header.php
+++ projeto_filme/templates/header.php
@@ -1,50 +1,49 @@
 <?php
-    include_once("../config/url.php");
+include_once("../config/url.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <title>Avaliação Filmes</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/js/bootstrap.bundle.min.js" 
-    integrity="sha512-BOsvKbLb0dB1IVplOL9ptU1EYA+LuCKEluZWRUYG73hxqNBU85JBIBhPGwhQl7O633KtkjMv8lvxZcWP+N3V3w==" crossorigin="anonymous"
-    referrerpolicy="no-referrer"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" 
-    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous"
-    referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="<?= $BASE_URL?>/../css/style.css">
-    <link rel="short icon" href="<?= $BASE_URL?>/../img/moviestar.ico"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/js/bootstrap.bundle.min.js"
+        integrity="sha512-BOsvKbLb0dB1IVplOL9ptU1EYA+LuCKEluZWRUYG73hxqNBU85JBIBhPGwhQl7O633KtkjMv8lvxZcWP+N3V3w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/../css/style.css">
+    <link rel="short icon" href="<?= $BASE_URL ?>/../img/moviestar.ico" />
 
     <header>
         <nav id="main-navbar" class="navbar navbar-expand-lg">
-            <a href="<?= $BASE_URL ?>"class="nav-brand">
-            <img src="<?= $BASE_URL ?>/../img/logo.svg" alt="MovieStar" id="logo">
-            <span id="moviestar-title">Filmes premiados</span>
-</a>
-        <button class="navbar-toffle" type="button" data-toggle="collapse"
-        data-target="#navbar" arial-control="navbar" arial-expanded="false" arial-label="Toggle navigation">
-        <i class="fas fa-bars"></i> 
-        </button>
-
-        <form action="#" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-            <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search"
-            placeholder="Buscar filmes" arial-labrel="Search">
-            <button class="btn my-2 mr-sm-0" type="submit">
-                <i class="fas da-search"></i>
-            </button>     
-        </form>
-
-        <div class="collapse navbar-collapse" id="navbar">
-            <ul class="navbar-nav">
-                <li class="navbar-item">
-                    <a href="#" class="nav-link">Entrar / Cadastrar</a>
-                </li>
-
-            </ul>
-
-        </div>
+            <a href="<?= $BASE_URL ?>" class="navbar-brand">
+                <img src="<?= $BASE_URL ?>/../img/logo.svg" alt="MovieStar" id="logo">
+                <span id="moviestar-title">Filmes Premiados</span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar"
+                aria-controls="navbar" aria-expanded="false" aria-label="Toggle
+navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <form action="#" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
+                <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search"
+                    placeholder="Buscar Filmes" aria-label="Search">
+                <button class="btn my-2 my-sm-0" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Entrar / Cadastrar</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </header>
 </head>
+
 <body>
